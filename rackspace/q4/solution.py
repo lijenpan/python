0c7b3430bd956ed095aab4888bb0ab38d84fb063
lijenpan <adam.pan@live.com>
2014-05-30 20:02:00 -0400

    Python almost in-place version.
    This is purely for the sake of the exercise because MutableString
        is deprecated in Python 3. The proper solution still needs to be in
        C/C++.

--- rackspace/q4/solution.py
+++ rackspace/q4/solution.py
@@ -1,10 +1,10 @@
-import re
-import UserString
+from UserString import MutableString
 
 
 def strip_whitespace_and_adjacent_duplicate_letter(text):
-    """Returns string without whitespace and with
-    duplicate characters if they are next to each other.
+    """This is purely for the sake of the exercise because MutableString
+    is deprecated in Python 3. The proper solution still needs to be in
+    C/C++.
 
     >>> strip_whitespace_and_adjacent_duplicate_letter("abb cddpddef gh")
     'abcdpdefgh'
@@ -12,9 +12,23 @@ def strip_whitespace_and_adjacent_duplicate_letter(text):
     >>> strip_whitespace_and_adjacent_duplicate_letter("aaaadam mmmm")
     'adam'
     """
-    mutable_text = UserString.MutableString(text)
-    return re.sub(r'([a-z])\1+', r"\1", ''.join(mutable_text.split()))
+    mutable_text = MutableString(text)
+    num_of_whitespace = mutable_text.count(' ')
 
+    for i in xrange(0, num_of_whitespace):
+        mutable_text.remove(' ')
+    
+    mutable_text_length = len(mutable_text)
+    i = 0
+    
+    while i < mutable_text_length - 1:
+        if mutable_text[i] == mutable_text[i + 1]:
+            del mutable_text[i + 1]
+            mutable_text_length -= 1
+            i = 0
+        else:
+            i += 1
+    return mutable_text
 
 if __name__ == '__main__':
     import doctest
