798f69622bc8842742b7e1907987e57cd53e9a52
Adam <adam.pan@live.com>
2016-08-15 13:56:41 -0500

    Create 12_integer_to_roman.py

---
+++ leetcode/12_integer_to_roman.py
@@ -0,0 +1,7 @@
+"""
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+===============================
+I actually worked on a similar problem: https://github.com/lijenpan/python/blob/master/advanceddigital/roman_numeral.py
+"""
