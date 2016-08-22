87296f65a7f74074966af8243198a9b3cc6b1de2
Adam <adam.pan@live.com>
2016-08-22 13:31:58 -0500

    Create 57_insert_interval.py

---
+++ leetcode/57_insert_interval.py
@@ -0,0 +1,40 @@
+"""
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+==================================
+If you understand Q56, this should be trivial.
+"""
+
+
+# Definition for an interval.
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        intervals.append(newInterval)
+        intervals = sorted(intervals, key=lambda x: x.start)
+        res = []
+        for i in intervals:
+            if len(res) == 0 or res[-1].end < i.start:
+                res.append(i)
+            else:
+                res[-1].end = max(res[-1].end, i.end)
+        return res
