537cb9cb06a755ce1935291f3f841ad70d2fa742
Adam <adam.pan@live.com>
2016-10-17 15:09:37 -0500

    Create 77_combinations.py

---
+++ leetcode/77_combinations.py
@@ -0,0 +1,43 @@
+"""
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+For example,
+If n = 4 and k = 2, a solution is:
+
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+==============================
+Cannot use itertools.combinations...
+"""
+
+def combination(iterable, r):
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = range(r)
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
+
+
+def combine(n, k):
+    return [x for x in combination(range(1, n + 1), k)]
+
+
+if __name__ == "__main__":
+    print combine(4, 2)
