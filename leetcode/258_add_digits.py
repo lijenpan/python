f898510c295fb4d807b075ba158f726e3c5ed765
Adam <adam.pan@live.com>
2016-10-18 12:41:38 -0500

    Create 258_add_digits.py

---
+++ leetcode/258_add_digits.py
@@ -0,0 +1,23 @@
+"""
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+==============================
+Digital root.
+"""
+
+
+def addDigits(num):
+    """
+    :type num: int
+    :rtype: int
+    """
+    if num == 0:
+        return 0
+    else:
+        return 1 + (num - 1) % 9
