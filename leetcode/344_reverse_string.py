9660c4bc8298b9d1f582b85383eba3299b4d0dd1
Adam <adam.pan@live.com>
2016-10-18 11:47:42 -0500

    Create 344_reverse_string.py

---
+++ leetcode/344_reverse_string.py
@@ -0,0 +1,16 @@
+"""
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+==============================
+Can't use reversed().
+"""
+
+
+def reverseString(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    return s[::-1]
