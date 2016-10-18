f59c5cd4e8baaeebd36a8dccafbe460dab3f498d
Adam <adam.pan@live.com>
2016-10-18 14:02:54 -0500

    Create 283_move_zeroes.py

---
+++ leetcode/283_move_zeroes.py
@@ -0,0 +1,30 @@
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of
+the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+==============================
+It can be as simple as for every 0 we pop off append it back. The caveat is you need two indices to do this right.
+"""
+
+
+def moveZeroes(nums):
+    """
+    :type nums: List[int]
+    :rtype: void Do not return anything, modify nums in-place instead.
+    """
+    i = 0
+    t = 0
+    while t < len(nums):
+        if nums[i] == 0:
+            nums.pop(i)
+            nums.append(0)
+            t = t + 1
+            i = i - 1
+        else:
+            t = t + 1
+        i = i + 1
