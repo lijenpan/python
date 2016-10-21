4646cd530333665c3deb613875ec417b9f0f72fc
Adam <adam.pan@live.com>
2016-10-21 13:16:40 -0500

    Create 121_best_time_to_buy_and_sell_stock.py

---
+++ leetcode/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,34 @@
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
+==============================
+Naive implementation would get Time Limit Exceeded.
+A slightly tweaked answer is tracking the lowest price and max price after that.
+"""
+
+
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    min_price = float("inf")
+    max_profit = 0
+    for i in xrange(0, len(prices)):
+        if prices[i] < min_price:
+            min_price = prices[i]
+        elif prices[i] - min_price > max_profit:
+            max_profit = prices[i] - min_price
+    return max_profit
