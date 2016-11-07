c2203fde372378eebdd8a03c4d29f33ac2c1a266
Adam <adam.pan@live.com>
2016-11-07 13:56:42 -0600

    Create 189_rotate_array.py

---
+++ leetcode/189_rotate_array.py
@@ -0,0 +1,37 @@
+"""
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+[show hint]
+
+Hint:
+Could you do it in-place with O(1) extra space?
+Related problem: Reverse Words in a String II
+===============================
+Without doing it in-place it's one liner. In-place is two liners. Even that is slow.
+The triple reverse technique is clever.
+"""
+
+
+def rotate(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: void Do not return anything, modify nums in-place instead.
+    """
+    def reverse(nums, start, end):
+        while start < end:
+            tmp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = tmp
+            start += 1
+            end -= 1
+
+    k %= len(nums)
+    reverse(nums, 0, len(nums) - 1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k, len(nums) - 1)
