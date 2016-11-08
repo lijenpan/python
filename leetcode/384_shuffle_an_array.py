69af0699190d8772c1e5a0b7ff6f9d45fc6dbdb7
Adam <adam.pan@live.com>
2016-11-08 11:24:30 -0600

    Create 384_shuffle_an_array.py

---
+++ leetcode/384_shuffle_an_array.py
@@ -0,0 +1,46 @@
+"""
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+===============================
+
+"""
+import random
+
+
+class Solution(object):
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        :type size: int
+        """
+        self.nums = nums
+        self.shuffled = nums[:]
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        random.shuffle(self.shuffled)
+        return self.shuffled
