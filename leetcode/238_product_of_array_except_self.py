b2c1ce8b4108153425d1fec01e34de694d0857b3
Adam <adam.pan@live.com>
2016-11-08 09:27:24 -0600

    Update 238_product_of_array_except_self.py

--- leetcode/238_product_of_array_except_self.py
+++ leetcode/238_product_of_array_except_self.py
@@ -1,5 +1,5 @@
 """
-iven an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product
 of all the elements of nums except nums[i].
 
 Solve it without division and in O(n).
