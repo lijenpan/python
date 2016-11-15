eb999d81307d66c3d34b54202e5436ef216b8324
Adam <adam.pan@live.com>
2016-11-15 14:24:10 -0600

    Create 394_decode_string.py

---
+++ leetcode/394_decode_string.py
@@ -0,0 +1,30 @@
+"""
+Given an encoded string, return it's decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly
+k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those
+repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Examples:
+
+s = "3[a]2[bc]", return "aaabcbc".
+s = "3[a2[c]]", return "accaccacc".
+s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+===============================
+I was writing a long answer with looking for left and right brackets. Then I saw this:
+https://discuss.leetcode.com/topic/57145/3-lines-python-2-lines-ruby-regular-expression
+"""
+
+
+def decodeString(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    while '[' in s:
+        s = re.sub(r'(\d+)\[([a-z]*)\]', lambda m: int(m.group(1)) * m.group(2), s)
+    return s
