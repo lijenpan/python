568a594af7587fbbf0c229a993c247de842ab18e
Adam <adam.pan@live.com>
2016-11-15 13:27:51 -0600

    Create 337_house_robber_iii.py

---
+++ leetcode/337_house_robber_iii.py
@@ -0,0 +1,44 @@
+"""
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called
+the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized
+that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked
+houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+     3
+    / \
+   2   3
+    \   \
+     3   1
+Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+     3
+    / \
+   4   5
+  / \   \
+ 1   3   1
+Maximum amount of money the thief can rob = 4 + 5 = 9.
+===============================
+Super detailed explanation: https://discuss.leetcode.com/topic/39834/step-by-step-tackling-of-the-problem/2
+"""
+
+
+def rob(root):
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+    def rob_next(root):
+        if root is None:
+            return [0, 0]
+        left = rob_next(root.left)
+        right = rob_next(root.right)
+        res = [0, 0]
+        res[0] = max(left[0], left[1]) + max(right[0], right[1])
+        res[1] = root.val + left[0] + right[0]
+        return res
+
+    res = rob_next(root)
+    return max(res[0], res[1])
