04681fae5c43ad91b4eac374a40a478e2a5ab016
Adam <adam.pan@live.com>
2016-12-02 13:47:58 -0600

    Create 146_LRU_Cache.py

---
+++ leetcode/146_LRU_Cache.py
@@ -0,0 +1,44 @@
+"""
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following
+operations: get and set.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity,
+it should invalidate the least recently used item before inserting a new item.
+===============================
+This is not hard in my book comparing the algorithm questions. Just need to push the least used cache out in set().
+"""
+from collections import OrderedDict
+
+
+class LRUCache(object):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        """
+        :rtype: int
+        """
+        try:
+            value = self.cache.pop(key)
+            self.cache[key] = value
+            return value
+        except KeyError:
+            return -1
+
+    def set(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: nothing
+        """
+        try:
+            self.cache.pop(key)
+        except KeyError:
+            if len(self.cache) >= self.capacity:
+                self.cache.popitem(last=False)
+        self.cache[key] = value
