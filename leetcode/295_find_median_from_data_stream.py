9740b48e52d5b821c93797708193092b1aa41514
Adam <adam.pan@live.com>
2016-12-02 14:06:39 -0600

    Create 295_find_median_from_data_stream.py

---
+++ leetcode/295_find_median_from_data_stream.py
@@ -0,0 +1,55 @@
+"""
+Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value.
+So the median is the mean of the two middle value.
+
+Examples:
+[2,3,4] , the median is 3
+
+[2,3], the median is (2 + 3) / 2 = 2.5
+
+Design a data structure that supports the following two operations:
+
+void addNum(int num) - Add a integer number from the data stream to the data structure.
+double findMedian() - Return the median of all elements so far.
+For example:
+
+add(1)
+add(2)
+findMedian() -> 1.5
+add(3)
+findMedian() -> 2
+===============================
+First you need to be mindful about integer division will not return floating points.
+Second the test cases forces you to optimize so single list is not good enough.
+
+You can find out more about this algorithm here: https://class.coursera.org/algo-003/lecture/62/
+"""
+from heapq import heappush, heappushpop, heappop
+
+class MedianFinder:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.heaps = [], []
+
+    def addNum(self, num):
+        """
+        Adds a num into the data structure.
+        :type num: int
+        :rtype: void
+        """
+        small, large = self.heaps
+        heappush(small, -heappushpop(large, num))
+        if len(large) < len(small):
+            heappush(large, -heappop(small))
+
+    def findMedian(self):
+        """
+        Returns the median of current data stream
+        :rtype: float
+        """
+        small, large = self.heaps
+        if len(large) > len(small):
+            return float(large[0])
+        return (large[0] - small[0]) / 2.0
